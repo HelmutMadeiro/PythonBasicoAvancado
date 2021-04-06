d1c2987cd9d33bb4575ef37de6b8349c822d0b0c
Helmut Madeiro <HelmutMadeiro@users.noreply.github.com>
2021-04-05 23:24:07 -0300

    Update Advinhacao.py

--- Jogo/Advinhacao.py
+++ Jogo/Advinhacao.py
@@ -1,16 +1,16 @@
 print("**********************************")
-print("bem vindo no jogo da adivinhacao ")
+print("bem vindo no jogo da adivinhação ")
 print("**********************************")
 
 numero_secreto = 42
 
-chute_str = input("Digite o seu numero: ")
+chute_str = input("Digite o seu número: ")
 
 print("voce chutou ", chute_str)
 
 chute = int(chute_str)
 
 if numero_secreto == chute:
-    print("Voce acertou")
+    print("Voce acertou!!!")
 else:
     print("Voce errou")
