d4408300fefee8b4ed16301ad11f557153ef35d5
Helmut Madeiro <HelmutMadeiro@users.noreply.github.com>
2021-04-11 16:58:22 -0300

    .

--- OrientacaoObjeto/conta.py
+++ OrientacaoObjeto/conta.py
@@ -14,21 +14,37 @@ class Conta:
     def deposita(self,valor):
         self.__saldo += valor
 
+    def __pode_sacar(self,valor_a_sacar):
+        valor_disponivel_a_sacar =  self.__saldo +self.__limite
+        return valor_a_sacar <= valor_disponivel_a_sacar
+
     def saca (self,valor):
-        self.__saldo -= valor
+        if __pode_sacar(self,valor_a_sacar):
+            self.__saldo -= valor
+        else:
+            print("O valor {} passou o limite".format(valor))
 
     def transfere(self,valor,destino):
         self.saca(valor)
         destino.deposita(valor)
 
-    def get_saldo(self):
+    @property
+    def saldo(self):
         return self.__saldo
-
-    def get_titular(self):
+    @property
+    def titular(self):
         return self.__titular
-
-    def get_limite(self):
+    @property
+    def limite(self):
         return self.__limite
-
-    def set_limite(self,limite):
+    @limite.setter
+    def limite(self,limite):
         self.__limite = limite
+
+    @staticmethod
+    def condigo_banco():
+      return "001"
+
+    @staticmethod
+    def condigos_bancos():
+        return {'BB':'001','Caixa': '104','Bradesco':'237'}
